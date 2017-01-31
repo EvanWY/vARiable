fdbf5c289cb0e5fa08deab3a311c1573301a3e77
EvanWY <wangyang.evan@gmail.com>
2017-01-30 19:47:42 -0500

    first demo rotation

--- vARiableUnityProject/Assets/Scripts/rotation3.cs
+++ vARiableUnityProject/Assets/Scripts/rotation3.cs
@@ -17,8 +17,11 @@ public class rotation3 : MonoBehaviour {
     public void driven_rotating(Vector3 mouse_velocity)
     {
         Debug.Log(mouse_velocity);
-        rotation_axis = Vector3.Cross(mouse_velocity, this.transform.parent.transform.forward).normalized;
-        angle_rotate = mouse_velocity.magnitude/3 *Time.fixedDeltaTime;
+        //rotation_axis = Vector3.Cross(mouse_velocity, this.transform.parent.transform.forward).normalized;
+        rotation_axis = this.transform.parent.forward;
+        //angle_rotate = mouse_velocity.magnitude/3 *Time.fixedDeltaTime;
+        angle_rotate = mouse_velocity.x / 3 *Time.fixedDeltaTime;
+
         rotation_for_frame = Quaternion.AngleAxis(angle_rotate, rotation_axis);
         this.transform.rotation = rotation_for_frame * this.transform.rotation;
 
