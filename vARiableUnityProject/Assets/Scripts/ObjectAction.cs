aac6667e98234b592d74936ed002063288956025
EvanWY <wangyang.evan@gmail.com>
2017-02-02 17:34:59 -0500

    add scale

---
+++ vARiableUnityProject/Assets/Scripts/ObjectAction.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectAction : MonoBehaviour {
+    Vector3 rotation_axis;
+    Quaternion rotation_for_frame;
+    float angle_rotate;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    public void DrivenRotating(Vector3 mouse_velocity)
+    {
+        Debug.Log(mouse_velocity);
+        //rotation_axis = Vector3.Cross(mouse_velocity, this.transform.parent.transform.forward).normalized;
+        rotation_axis = this.transform.parent.forward;
+        //angle_rotate = mouse_velocity.magnitude/3 *Time.fixedDeltaTime;
+        angle_rotate = mouse_velocity.x / 3 *Time.fixedDeltaTime;
+
+        rotation_for_frame = Quaternion.AngleAxis(angle_rotate, rotation_axis);
+        this.transform.rotation = rotation_for_frame * this.transform.rotation;
+
+    }
+
+    public void DrivenScaling(float scale)
+    {
+        transform.localScale *= scale;
+    }
+}
--- vARiableUnityProject/Assets/Scripts/mousemove.cs
+++ vARiableUnityProject/Assets/Scripts/mousemove.cs
@@ -13,24 +13,41 @@ public class mousemove : MonoBehaviour {
 	    
 	}
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        old_mouse_pos = mouse_pos;
-        mouse_pos = Input.mousePosition;
-        if (Input.GetMouseButton(0))
+        if (Input.touchCount == 2)
         {
-            if (old_mouse_pos != mouse_pos)
-            {
-                mouse_velocity = (mouse_pos - old_mouse_pos) / Time.fixedDeltaTime;
-                rotating_object.GetComponent<rotation3>().driven_rotating(mouse_velocity);
-            }
+            Touch touch0 = Input.GetTouch(0);
+            Touch touch1 = Input.GetTouch(1);
+
+            Vector2 touch0PrevPos = touch0.position - touch0.deltaPosition;
+            Vector2 touch1PrevPos = touch1.position - touch1.deltaPosition;
+
+            float prevDiff = (touch0PrevPos - touch1PrevPos).magnitude;
+            float currDiff = (touch0.position - touch1.position).magnitude;
+
+            float scale = currDiff / prevDiff;
+
+            rotating_object.GetComponent<ObjectAction>().DrivenScaling(scale);
         }
-        //Debug.Log(mouse_velocity.magnitude);
-        if (!(Input.GetMouseButton(0)))
+        else if (Input.GetMouseButton(0))
         {
             old_mouse_pos = mouse_pos;
+            mouse_pos = Input.mousePosition;
+            if (Input.GetMouseButton(0))
+            {
+                if (old_mouse_pos != mouse_pos)
+                {
+                    mouse_velocity = (mouse_pos - old_mouse_pos) / Time.fixedDeltaTime;
+                    rotating_object.GetComponent<ObjectAction>().DrivenRotating(mouse_velocity);
+                }
+            }
+            //Debug.Log(mouse_velocity.magnitude);
+            if (!(Input.GetMouseButton(0)))
+            {
+                old_mouse_pos = mouse_pos;
 
+            }
         }
     }
 }
