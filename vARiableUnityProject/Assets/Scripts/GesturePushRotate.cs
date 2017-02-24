512a62c95070bdc89eb6b7fc8f0b4070aa5ade46
EvanWY <wangyang.evan@gmail.com>
2017-02-23 19:48:22 -0500

    drone

---
+++ vARiableUnityProject/Assets/Scripts/GesturePushRotate.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GesturePushRotate : MonoBehaviour {
+
+	void OnTapAndHold()
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+    void OnTapAndHoldEnd()
+    {
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+    Vector3 deltaEular;
+    Vector3 currFrameEular;
+    void OnTapHoldAndDrag()
+    {
+        deltaEular = transform.localEulerAngles - currFrameEular;
+        currFrameEular = transform.localEulerAngles;
+
+        GetComponent<Rigidbody>().isKinematic = true;
+        Plane p = new Plane();
+        p.SetNormalAndPosition(transform.up, transform.position);
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float rayDistance;
+        if (p.Raycast(ray, out rayDistance))
+            transform.LookAt(ray.GetPoint(rayDistance));
+    }
+
+    void OnTapHoldAndDragEnd()
+    {
+
+
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Rigidbody>().angularVelocity = deltaEular / Time.deltaTime;
+    }
+}
